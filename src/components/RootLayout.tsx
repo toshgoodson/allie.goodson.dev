7a9e4d6fc5344f20bf6ec76e636b744041b5cf2d
Tosh Goodson <tosh.goodson@gmail.com>
2021-07-06 23:56:25 -0700

    clean up; set root font size to 1px and set bootstrap variables to compensate

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,27 +1,20 @@
 import React from 'react'
 import { Fonts } from './styles/Fonts'
 import { Grit } from './Grit'
-import { PageContent } from './PageContent'
-import { PageHeader } from './PageHeader'
 import { StyleBase } from './styles/StyleBase'
 import { PageFooter } from './PageFooter'
 import { RootHead } from './RootHead'
 
-type Props = {
-	h1: string
-}
+type Props = {}
 
 export const RootLayout: React.FunctionComponent<Props> = (props) => {
 	return <div>
 		<RootHead/>
 		<Fonts/>
 		<StyleBase/>
-		<PageHeader>
-			<h1>{props.h1}</h1>
-		</PageHeader>
-		<PageContent>
+		<div>
 			{props.children}
-		</PageContent>
+		</div>
 		<PageFooter/>
 		<Grit/>
 	</div>
--- src/components/styles/StyleBase.ts
+++ src/components/styles/StyleBase.ts
@@ -3,13 +3,12 @@ import { Palette } from "../../models/Palette"
 
 export const StyleBase = createGlobalStyle`
 	body {
-		background: ${Palette.pickledBluewood};
 		position: relative;
 	}
 
 	@media (min-width: 1200px) {
 		.container {
-			max-width: 1000px;
+			max-width: 970px;
 		}
 	}
 `
