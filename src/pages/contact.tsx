962d291281f90cf3f3f07a3cc7d0609791b69ba4
Tosh Goodson <tosh.goodson@gmail.com>
2021-07-12 01:27:58 -0700

    swoop contact link

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -5,9 +5,36 @@ import { ContentContainer } from '../components/layout/ContentContainer'
 import { RootLayout } from '../components/RootLayout'
 import { rfs } from '../models/rfs'
 
+const Swoop = styled.span`
+	display: inline-block;
+	position: relative;
+
+	&:after {
+		background: linear-gradient(to right, ${({theme}) => theme.colors[2][0]}, ${({theme}) => theme.colors[2][0]}) no-repeat right;
+		background-size: 0% auto;
+		content: '';
+		height: 1em;
+		left: 0.25em;
+		position: absolute;
+		top: -0.05em;
+		transition: background-size 0.15s ease-out, background-position 0s ease-out;
+		width: 100%;
+		z-index: -1;
+	}
+`
 const Email = styled(Brand)`
 	color: ${({theme}) => theme.colors[1]};
 	${rfs.fontSize('44px')}
+
+	a { 
+		color: inherit !important;
+	
+		&:hover ${Swoop}:after {
+			background-position: left;
+			background-size: 100% auto;
+		}
+	}
+	
 `
 const ColorSection = styled.div`
 	background: ${({theme}) => theme.colors[3]};
@@ -23,7 +50,7 @@ const Subheader = styled.div`
 export default function Contact() {
 	return <RootLayout>
 		<ContentContainer>
-			<Email as="h1">allie@goodson.dev</Email>
+			<Email as="h1"><a href="mailto:allie@goodson.dev" target="_blank"><Swoop>allie@goodson.dev</Swoop></a></Email>
 		</ContentContainer>
 		<ColorSection>
 			<ContentContainer>
