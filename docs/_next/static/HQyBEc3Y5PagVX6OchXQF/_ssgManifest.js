c651759270c1268eaa184b972b685daabb1b5d29
Tosh Goodson <tosh.goodson@gmail.com>
2024-03-06 09:44:44 -0800

    release; use compressed pdf

---
+++ docs/_next/static/HQyBEc3Y5PagVX6OchXQF/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fcontact","\u002Fcopyright","\u002Fabout","\u002Fmarshawarma","\u002Fclean-sf","\u002Fboss-startup-science","\u002Fmultimedia"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
