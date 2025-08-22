c4949bbd7e36399c290197c0ca85f89b42f2476b
Tosh Goodson <tosh.goodson@gmail.com>
2025-08-21 22:33:49 -0400

    release

--- docs/_next/static/2y6nNFLEF22TrFvi38clC/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fcopyright","\u002Fmarshawarma","\u002Fclean-sf","\u002Fboss-startup-science","\u002Fcontact","\u002Fmultimedia"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
